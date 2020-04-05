a93fe98d4e8c015ce7d65df0fa2f465056386d0e
enricopolanski <enricopolanski@gmail.com>
2020-04-05 11:59:10 +0200

    feat(0122): TypeScript solution

---
+++ typescript/problems/0122/solution.ts
@@ -0,0 +1,20 @@
+// Runtime: 56ms. Beats 80.41 % of javascript submissions.
+
+var maxProfit = function (prices: number[]): number {
+    let min = prices[0];
+    let max = prices[0];
+    let profit = 0;
+    for (let i = 1; i < prices.length; ++i) {
+        if (prices[i] > max) {
+            max = prices[i];
+        } else if (prices[i] < max || prices[i] <= min) {
+            profit += max - min;
+            min = prices[i];
+            max = prices[i]
+        }
+    }
+    if (max > min) {
+        profit += max - min
+    }
+    return profit
+};
