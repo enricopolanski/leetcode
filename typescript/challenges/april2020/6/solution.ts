f97a4250a88de267bd51646e0be6f70eefba130a
enricopolanski <enricopolanski@gmail.com>
2020-04-06 09:53:59 +0200

    feat(april-challenge-06): TypeScript solution

---
+++ typescript/challenges/april2020/6/solution.ts
@@ -0,0 +1,16 @@
+// Runtime: 132 ms, faster than 57.21% of JavaScript online submissions for Group Anagrams.
+
+var groupAnagrams = function (strs: string[]): string[][] {
+    const uniques = {};
+    const toSortedStr = str => str.split('').sort().join('')
+    for (let str of strs) {
+        const sorted = toSortedStr(str)
+        if (sorted in uniques) {
+            uniques[sorted].push(str)
+        } else {
+            uniques[sorted] = [str]
+        }
+    }
+    // any needed because we're targetting es2017 but actually compiling for es2015
+    return (Object as any).values(uniques)
+};
