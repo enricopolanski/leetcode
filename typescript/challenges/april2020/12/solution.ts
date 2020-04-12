c496bc4b594b024bb1aec7a7af7c5af0d048e57a
enricopolanski <enricopolanski@gmail.com>
2020-04-12 16:30:29 +0200

    feat(april-challenge-12): TypeScript solution

---
+++ typescript/challenges/april2020/12/solution.ts
@@ -0,0 +1,14 @@
+type NonEmptyArray<A> = [A, ...A[]]
+
+var lastStoneWeight = function (stones: NonEmptyArray<number>): number {
+  const reverseSort = arr => arr.sort((a, b) => b - a) as NonEmptyArray<number>
+  let reverseSorted = reverseSort(stones);
+  while (reverseSorted.length > 1) {
+    const diff = reverseSorted[0] - reverseSorted[1];
+    const [first, second, ...rest] = reverseSorted
+    reverseSorted = reverseSort([...rest, diff])
+  }
+  return reverseSorted[0];
+};
+
+export default lastStoneWeight
