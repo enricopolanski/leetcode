49a0918e5e76e002d109cc6a1902be73742f9ca5
enricopolanski <enricopolanski@gmail.com>
2020-04-16 11:03:38 +0200

    feat(0678): TypeScript solution

---
+++ typescript/problems/0678/solution.ts
@@ -0,0 +1,29 @@
+// TODO: Restudy well with differet approaches (brute, dp, etc)
+
+var checkValidString = function (s: string): boolean {
+  let l = 0;
+  let h = 0;
+
+  for (let char of s) {
+    if (char === '(') {
+      h++;
+      l++;
+      continue;
+    }
+
+    if (char == ')' && h == 0) {
+      return false;
+    }
+
+    if (char == ')') {
+      h--;
+      l = Math.max(0, l - 1);
+      continue;
+    }
+
+    h++;
+    l = Math.max(0, l - 1);
+  }
+
+  return l === 0;
+};
