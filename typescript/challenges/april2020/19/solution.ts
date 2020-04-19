fa43cb800b93396ccde4460ab006a57c18e6fcb7
enricopolanski <enricopolanski@gmail.com>
2020-04-19 13:03:03 +0200

    fix(april-challenge-19): added export to search

--- typescript/challenges/april2020/19/solution.ts
+++ typescript/challenges/april2020/19/solution.ts
@@ -1,5 +1,5 @@
 // TODO: find more idiomatics (this still beats 95% of JS implementations)
-var search = function (nums: number[], target: number): number {
+export var search = function (nums: number[], target: number): number {
   const sol = nums.indexOf(target)
   return sol !== null ? sol : -1
 };
